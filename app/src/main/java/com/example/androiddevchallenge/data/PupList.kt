a95df94c83ab6e825e2ec0ed9a2a676a31ba5326
Prateek Rai <codes.prateek@gmail.com>
2021-03-01 23:59:23 +0530

    Dog added

--- app/src/main/java/com/example/androiddevchallenge/data/PupList.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/PupList.kt
@@ -11,10 +11,10 @@ object PupList {
 
 private val petList = listOf(
     Puppy(
-        "Bella",
+        "Rocco",
         "2",
         "Female",
-        "Chow Chow",
+        "German Shefard",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
         R.drawable.german_shefard
@@ -23,64 +23,82 @@ private val petList = listOf(
         "Max",
         "2",
         "Female",
-        "Siberian Husky",
+        "White Labrador",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
-        R.drawable.blue_dog
+        R.drawable.labrador_puppy
     ),
     Puppy(
         "Tommy",
         "2",
         "Female",
-        "Golden Retriever",
+        "Blue Dog",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
         R.drawable.blue_dog
     ),
     Puppy(
-        "Tommy",
+        "Jake",
         "2",
         "Female",
-        "Another",
-        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
-        "New York",
-        R.drawable.blue_dog
-    ),
-    Puppy(
-        "Sheru",
-        "2",
-        "Female",
-        "Not Decided",
-        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
-        "New York",
-        R.drawable.blue_dog
-    ),
-    Puppy(
-        "Zammy",
-        "2",
-        "Male",
-        "Ypp",
+        "Red Dog",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
         R.drawable.red_dog
     ),
     Puppy(
-        "Zacky",
+        "Leo",
         "2",
-        "Male",
-        "White Lion",
+        "Female",
+        "Orange Dog",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
         R.drawable.orange_dog
     ),
     Puppy(
-        "Pillu",
+        "Coco",
         "2",
         "Male",
-        "Husky",
+        "Golden Retriever",
         "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
         "New York",
-        R.drawable.orange_dog
+        R.drawable.golden_retriever
+    ),
+    Puppy(
+        "Shadow",
+        "2",
+        "Male",
+        "Pug Breed",
+        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
+        "New York",
+        R.drawable.pug_puppy
+    ),
+    Puppy(
+        "Mickey",
+        "2",
+        "Male",
+        "Pomerian Breed",
+        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
+        "New York",
+        R.drawable.pomerian
+    ),
+    Puppy(
+        "Oreo",
+        "2",
+        "Male",
+        "Boxer Breed",
+        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
+        "New York",
+        R.drawable.boxer_puppy
+    ),
+    Puppy(
+        "Oscar",
+        "2",
+        "Male",
+        "St. Benard",
+        "Bella is a sweet puppy. She has been at shelter since she was born and we'd really love to see her get into a new home soon!. She gets along well with other pets, and is used to being around dogs.",
+        "New York",
+        R.drawable.benard_puppy
     ),
 
 )
